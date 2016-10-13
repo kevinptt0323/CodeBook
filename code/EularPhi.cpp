b673069a2673df7888f1f606a9ff5463dc11395f
Kevin Lin <kevinptt0323@gmail.com>
2016-10-14 01:41:07 +0800

    fix bug in eular's phi

--- code/EularPhi.cpp
+++ code/EularPhi.cpp
@@ -4,7 +4,7 @@ memset(phi, 0, sizeof(phi));
 for(int i=2; i<MAXN; ++i) {
 	if( phi[i]==0 ) prime[pn++]=i, phi[i]=i-1;
 	for(int j=0; j<pn; ++j) {
-		if( i*prime[j]>=MAXN ) break;
+		if( i*prime[j]>MAXN ) break;
 		if( i%prime[j]==0 ) {
 			phi[i*prime[j]] = phi[i] * prime[j];
 			break;

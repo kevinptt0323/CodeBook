f777a39b28c5064969cd45ff7dda298f61eb257d
Kevin Lin <kevinptt0323@gmail.com>
2016-10-14 01:40:14 +0800

    update Gaussian Elimination

--- code/GaussianElimination.cpp
+++ code/GaussianElimination.cpp
@@ -1,29 +1,30 @@
 // default for module version, comments for double version
-// double mmap[row][column];
+//double mmap[row][column];
 const ll modn = 1000000007;
 ll mmap[row][column];
-ll inv(ll b) {
+ll inv(ll b)
+{
 	return (b==1)?1:inv(modn%b)*(modn-modn/b)%modn; 
 }
-void gauss(int n,int m) {
+void gauss(ll mat[row][column],int n,int m)
+{
 	int k=0;
 	for(int i=0; i<m; i++)
 		for(int j=k; j<n; j++)
-			if(mmap[j][i]!=0) {
+			if(mat[j][i]!=0){
 				for(int l=i; l<m; l++)
-					swap(mmap[k][l],mmap[j][l]);
+					swap(mat[k][l],mat[j][l]);
 				for(j++; j<n; j++){
-					if(mmap[j][i]==0)
+					if(mat[j][i]==0)
 						continue;
-					//double scale=mmap[j][i]/mmap[k][i];
-					ll scale=mmap[j][i]*inv(mmap[k][i])%modn;
-					for(int p=i+1; p<n; p++)
-						//mmap[j][p]-=mmap[k][p]*scale;
-						mmap[j][p]=(mmap[j][p]-mmap[k][p]*scale%modn+modn)%modn;
-					mmap[j][i]=0;
+					//double scale=mat[j][i]/mat[k][i];
+					long long scale=mat[j][i]*inv(mat[k][i])%modn;	//mod version
+					for(int p=i+1; p<m; p++)
+						//mat[j][p]-=mat[k][p]*scale;
+						mat[j][p]=(mat[j][p]-mat[k][p]*scale%modn+modn)%modn;
+					mat[j][i]=0;
 				}
 				k++;
 				break;
 			}
 }
-

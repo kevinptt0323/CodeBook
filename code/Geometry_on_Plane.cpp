871f200a31d3f2fb6d228258f2f2e02d607d2a18
Kevin Lin <kevinptt0323@gmail.com>
2016-11-23 22:19:52 +0800

    modify indent and spacing

--- code/Geometry_on_Plane.cpp
+++ code/Geometry_on_Plane.cpp
@@ -1,23 +1,23 @@
 struct node {
 	double x,y;
-	node() {}
-	node(double _x, double _y) : x(_x),y(_y) {}
-	node operator+(const node& rhs)const
-		{ return node(x+rhs.x,y+rhs.y); }
-	node operator-(const node& rhs)const
-		{ return node(x-rhs.x,y-rhs.y); }
-	node operator*(const double& rhs)const
-		{ return node(x*rhs,y*rhs); }
-	node operator/(const double& rhs)const
-		{ return node(x/rhs,y/rhs); }
-	double operator*(const node& rhs)const
+	node(double _x=0, double _y=0) : x(_x),y(_y) {}
+	node operator+(const node& rhs) const
+		{ return node(x+rhs.x, y+rhs.y); }
+	node operator-(const node& rhs) const
+		{ return node(x-rhs.x, y-rhs.y); }
+	node operator*(const double& rhs) const
+		{ return node(x*rhs, y*rhs); }
+	node operator/(const double& rhs) const
+		{ return node(x/rhs, y/rhs); }
+	double operator*(const node& rhs) const
 		{ return x*rhs.x+y*rhs.y; }
-	double len2()const{ return x*x+y*y; }
-	double len()const{ return sqrt(x*x+y*y); }
-	node unit()const{ return *this/len(); }
-	double operator^(const node& rhs)const{ return x*rhs.y-y*rhs.x; }
-	node T()const{ return node(-y,x); }
-	node rot(double rad)const{ //逆時針旋轉 弧度
+	double operator^(const node& rhs) const
+		{ return x*rhs.y-y*rhs.x; }
+	double len2() const { return x*x+y*y; }
+	double len() const { return sqrt(x*x+y*y); }
+	node unit() const { return *this/len(); }
+	node T() const { return node(-y,x); }
+	node rot(double rad) const { // rotate counter-clockwise in rad
 		return node(cos(rad)*x-sin(rad)*y, sin(rad)*x+cos(rad)*y);
 	}
 };
@@ -25,10 +25,10 @@ node __mirror(node normal, double constant, node point){ //2D3D
 	double scale=(normal*point+constant)/(normal*normal);
 	return point-normal*(2*scale);
 }
-node mirror(node p1, node p2, node p3){ //2D3D
-	return __mirror((p2-p1).T(),(p2-p1).T()*p1*(-1),p3);
+node mirror(node p1, node p2, node p3){ // 2D3D
+	return __mirror((p2-p1).T(), (p2-p1).T()*p1*(-1), p3);
 }
-double ori(const node& p1,const node& p2, const node& p3){ //平行四邊形面積(帶正負)
+double ori(const node& p1, const node& p2, const node& p3){ //平行四邊形面積(帶正負)
 	return (p2-p1)^(p3-p1);
 }
 bool intersect(const node& p1, const node& p2, const node& p3, const node& p4){

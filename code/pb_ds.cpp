eb30a60079ba7b6a154482730658ad0370623c00
Kevin Lin <kevinptt0323@gmail.com>
2016-11-25 05:01:50 +0800

    add pb_ds trie

--- code/pb_ds.cpp
+++ code/pb_ds.cpp
@@ -6,6 +6,13 @@ using namespace __gnu_pbds;
 typedef __gnu_pbds::priority_queue<T, greater<T>, pairing_heap_tag> Heap;
 /*
  * method: push, pop, modify(iter, val), erase, join
+ * point_iterator push(const_reference r_val)
+ * pop
+ * void modify(point_iterator it,const_reference r_new_val)
+ * size_type erase_if(Pred prd) - return earsed number
+ * void join(priority_queue &other)
+ * void split(Pred prd, priority_queue &other) - move v into other if prd(v)
+ *
  * tags: pairing_heap_tag, binary_heap_tag, binomial_heap_tag, rc_binomial_heap_tag, thin_heap_tag
  */
 
@@ -13,9 +20,18 @@ typedef __gnu_pbds::priority_queue<T, greater<T>, pairing_heap_tag> Heap;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> RBTree;
 typedef tree<int, null_type, less<int>, splay_tree_tag, tree_order_statistics_node_update> Splay;
 /*
- * point_iterator find_by_order(size_type order)
+ * point_iterator find_by_order(size_type order) [0, size)
  * size_type order_of_key(const_key_reference r_key) - number of elements < r_key
  * void split(const_key_reference r_key, tree &other) - move elements > r_key
+ */
+
+#include<ext/pb_ds/trie_policy.hpp>
+#include<ext/pb_ds/tag_and_trait.hpp>
+typedef trie<string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update> Trie;
+typedef trie<string, null_type, string_trie_e_access_traits, pat_trie_tag, trie_prefix_search_node_update> Trie;
+/*
+ * pair<Trie::iterator, bool> insert(string s) - iterator and is new string
+ * pair<Trie::iterator, Trie::iterator> prefix_range(string pre)
  */
 
 #include <ext/pb_ds/hash_policy.hpp>

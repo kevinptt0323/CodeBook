f4ecdc964a8029dd808d8b56da0aca888848184b
Kevin Lin <kevinptt0323@gmail.com>
2017-05-05 23:04:16 +0800

    fix bit range modify

--- code/BIT_range_modify.cpp
+++ code/BIT_range_modify.cpp
@@ -3,10 +3,10 @@ template<class T>
 class fenwick {
 public:
 	fenwick(int __size=SIZE) {
-		size = __size+10;
-		a = new T[size], b=new T[size];
-		memset(a, 0, sizeof(T)*size);
-		memset(b, 0, sizeof(T)*size);
+		size = __size;
+		a = new T[size+1], b=new T[size+1];
+		memset(a, 0, sizeof(T)*(size+1));
+		memset(b, 0, sizeof(T)*(size+1));
 	}
 	~fenwick() { delete[] a, delete[] b;}
 	inline void add(int l, int r, long long n) {
@@ -17,7 +17,7 @@ public:
 private:
 	int size;
 	T *a, *b;
-	inline void __add(T *arr, int x, T n) { for(; x&&n&&x<size; x+=lowbit(x)) arr[x]+=n; }
+	inline void __add(T *arr, int x, T n) { for(; x&&n&&x<=size; x+=lowbit(x)) arr[x]+=n; }
 	inline T __sum(T x) { return __sum(a, x)+(__sum(b, size)-__sum(b, x))*x; }
 	inline T __sum(T *arr, int x) {
 		T res=0;
